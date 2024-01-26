da6d1fff5659a72ac0b953f9a3fe7ec3386ba4dd
Abhishek Chauhan <abhishekchauhan1509@gmail.com>
2024-01-26 17:27:13 +0530

    added pagination + scheduler for api fetching

--- app.py
+++ app.py
@@ -28,7 +28,36 @@ if mongo.db is not None:
 def test_connection():
     return jsonify({'message': 'Hello!'})
 
+# API to return paginated videos
+@app.route('/videos', methods=['GET'])
+def get_paginated_videos():
+    page = int(request.args.get('page', 1))
+    per_page = int(request.args.get('per_page', 10))
 
+    if mongo.db is not None:
+        videos = mongo.db.videos.find().sort('published_datetime', -1).skip((page - 1) * per_page).limit(per_page)
+        videos_list = json_util.loads(json_util.dumps(videos, default=lambda x: x.__dict__))
+
+        return jsonify({'videos': videos_list})
+    else:
+        return jsonify({'error': 'MongoDB connection not established'})
+
+
+def create_video_list_response(videos):
+    """
+    Create a response for paginated videos.
+    """
+    return [{
+        'title': video['title'],
+        'description': video['description'],
+        'published_datetime': video['published_datetime'].isoformat(),
+        'thumbnails': video['thumbnails'],
+        'video_id': video['video_id'],
+    } for video in videos]
+    
 
 if __name__ == '__main__':
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(fetch_and_store_videos, 'interval', seconds=10)
+    scheduler.start()
     app.run(debug=True)
